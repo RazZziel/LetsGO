f2e2d9eef4eeeece5b31b617f069470c763c4431
Ismael Barros² <razielmine@gmail.com>
2015-07-22 00:16:20 +0200

    Filter games and moves publications

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,24 @@
 "use strict";
 
-Meteor.publish("games", function() {
-	return Games.find({});
+Meteor.publish("games", function(gameId) {
+
+	if (gameId) {
+		return Games.find(gameId);
+	} else {
+		return Games.find(
+			{$or : [
+				{ p1: this.userId },
+				{ p2: this.userId }
+			]},
+			{
+				sort: {created: 1},
+				limit: 1
+			}
+		);
+	}
 });
 
-Meteor.publish("moves", function() {
-	return Moves.find({});
+Meteor.publish("moves", function(gameId) {
+
+	return Moves.find({ gameId: gameId });
 });
